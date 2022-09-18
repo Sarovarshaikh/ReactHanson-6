6ebe10b2e95e3c4e15e31864585a6e719e3acff9
Sarovar Shaikh <sarwarshaikh3622@gmail.com>
2022-09-18 22:42:30 +0530

    Todo list in React

---
+++ src/components/Home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './Compo.css'
+function Home() {
+    return (
+      <div>
+        <center>
+          <h1 className="Home">Hello</h1>
+          
+        </center>
+      </div>
+    );
+  }
+
+  export default Home;
